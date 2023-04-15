c0ac47915d30cc29a757d24477d0e98374d23d68
ParkSolji <psj020624@gmail.com>
2023-04-16 02:38:50 +0900

    numbaseball

--- task/numbaseball.js
+++ task/numbaseball.js
@@ -1,7 +1,9 @@
 var digit=0;
+const $checkVal3 = document.getElementById('digit3');
+const $checkVal4 = document.getElementById('digit4');
+
 function deterDigit(){
-const $checkVal = document.getElementById('digit3');
-if ($checkVal.checked == true){
+if ($checkVal3.checked == true){
   digit=digit+3;
 }
 else{
@@ -9,8 +11,8 @@ else{
 }
 }
 
-
 function makeRandom(){
+  $gameStart.disabled=true;
   deterDigit();
   for (let i=1; i<digit+1; i++){
     randomNum = Math.floor(Math.random()*(9-1)+1);
@@ -20,6 +22,12 @@ function makeRandom(){
     else{
       numList.push(randomNum);
     }
+  }
+  if ($checkVal3.checked == true){
+    $checkVal4.disabled=true;
+  }
+  else{
+    $checkVal3.disabled=true;
   }
   $inputSet.style.display = 'block';
   $startText.style.display = 'block';
@@ -35,6 +43,7 @@ const $num = document.getElementById('number')
 const $inputSet = document.getElementById('inputSet');
 const $btn = document.getElementById('submit');
 const $inputNumber = document.getElementById('input');
+const $restart = document.getElementById('restart');
 
 $inputNumber.addEventListener("keyup",function(event){
   if(event.keyCode==13){
@@ -43,9 +52,12 @@ $inputNumber.addEventListener("keyup",function(event){
   }
 });
 
+
 function guessNumber(){
   const table = document.getElementById('result');
   const newRow = table.insertRow();
+  const input= $inputNumber.value.split('');
+  const set = new Set(input);
   if (numList==''){
     alert("게임 시작 버튼을 눌러주세요!");
     $inputNumber.value = "";
@@ -53,31 +65,47 @@ function guessNumber(){
   else if ($inputNumber.value.length != digit){    
     alert(`${digit}자리 숫자를 입력해 주세요.`);
   }
-
+  else if(input.length !== set.size){
+    alert("중복된 숫자가 있어요!");
+    $inputNumber.value = "";
+  }
   else{
     step++;
   let ball=0;
   let strike=0;
   //input1.value
-  const input= $inputNumber.value.split('');
   // console.log(input);
   //document.write(input1);
-  for (let i=0; i<3; i++){
-    if (numList.join('') == input.join('')){
-      alert(`[정답 : ${input.join('')}] [점수 : ${10-step}점]   홈런!`);
+  if (numList.join('') == input.join('')){
+    alert(`[정답 : ${input.join('')}] [점수 : ${10-step}점]   홈런!`);
+      const newCell1 = newRow.insertCell(0);
+      const newCell2 = newRow.insertCell(1);
+      const newCell3 = newRow.insertCell(2);
+      newCell1.style.color='red';
+      newCell2.style.color='red';
+      newCell3.style.color='red';
+      // Cell에 텍스트 추가
+      newCell1.innerText = `${step}`;
+      newCell2.innerText = `${input.join('')}`;
+      newCell3.innerText = `HomeRun`;
+      
+      $restart.style.display='block';
+      $restart.addEventListener('click',restart);        return;
       return;
     }
-    else if (numList[i] == input[i]){
+    else{
+    for (let i=0; i<digit; i++){
+    if (numList[i] == input[i]){
       strike++;
     }
     else{
-      for (let j=0; j<3; j++){
+      for (let j=0; j<digit; j++){
         if (numList[i] == input[j]){
           ball++;
         }
       }
     }
-  }
+  }}
 
   // printText=`${step} : ${input.join('')}\t[${strike}strike ${ball}ball] \t \n`;
   // textList.push(printText);
@@ -94,6 +122,27 @@ function guessNumber(){
   newCell3.innerText = `${strike}strike ${ball}ball`;
   $inputNumber.value = "";
 }
+}
+
+function restart(){
+  const table = document.getElementById('result');
+  $inputSet.style.display = 'none';
+  $startText.style.display = 'none';
+  for(let i=0; i < step; i++){
+     table.deleteRow(-1);
+  }
+    table.style.display='none'
+    $restart.style.display='none';
+    $inputNumber.value = "";
+    $checkVal3.disabled=false;
+    $checkVal4.disabled=false;
+    $gameStart.disabled=false;
+    digit=0
+    numList=[];
+    textList=[];
+    step = 0;
+    $gameStart.addEventListener('click',makeRandom);
+    $btn.addEventListener('click', guessNumber);
 }
 
 $gameStart.addEventListener('click',makeRandom);
